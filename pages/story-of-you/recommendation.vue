5347ae1aa9f624e5433eb1dec5ec5437401b3864
Knazran <khairulnazran94@gmail.com>
2022-10-29 18:11:31 +0800

    Finish recommendation page

---
+++ pages/story-of-you/recommendation.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="bg-hlb-primary-500 h-screen pt-12">
+
+        <!-- Traveller Card -->
+        <div class="flex  px-6">
+            <div class="w-1/3">
+                <img class="rounded border border-yellow-300" src="~/assets/img/travel.webp" />
+            </div>
+            <div class="w-2/3 pl-6">
+                <h2 class="text-md text-white font-semibold tracking-wider">The Avid Traveller</h2>
+                <p class="mt-2 text-white font-light text-xs italic">“Travelling to Vietnam in October during the
+                    monsoon season”</p>
+            </div>
+        </div>
+
+        <!-- Hawkie -->
+        <div class="mt-12 flex pl-6">
+            <h3 class="text-hlb-primary-900 text-md font-bold leading-tight">As an Avid Traveller, Hawkie thinks you
+                would love...</h3>
+            <img class="h-16 -translate-x-2 -translate-y-2" src="~/assets/img/pngwing 2.png" />
+        </div>
+
+        <!-- Emirates -->
+        <div class="flex flex-col items-center justify-center px-6">
+            <img class="" src="~/assets/img/emirates.png" />
+            <h3 class="text-hlb-primary-900 text-md font-bold leading-tight my-4">Emirates Credit Card</h3>
+            <p class="text-white font-light text-xs italic text-center">Enjoy complimentary flight to Europe, Skywards Miles on
+                daily spend, Emirates Skywards Silver membership and many more.</p>
+            
+            <div class="flex space-x-6 w-full items-start justify-center mt-6">
+                <button class="bg-white py-2 px-6 rounded-full text-hlb-primary-700 text-xs">More Details</button>
+                <button class="bg-hlb-secondary-500 py-2 px-6 rounded-full text-white text-xs">Apply Now</button>
+            </div>
+            <button class="bg-hlb-primary-700 py-2 px-12 rounded-full text-white my-8 w-full">See Next Story</button>
+
+        </div>
+    </div>
+</template>
+  
+<script>
+export default {
+    name: 'StoryOfYouRecommendationPage'
+}
+</script>
+  
+<style>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         'hlb-primary-500': '#4D64FF',
         'hlb-primary-700': '#293EC7',
+        'hlb-primary-900': '#030B56',
         'hlb-secondary-500': '#E30909'
       }
     },
