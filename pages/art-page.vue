39f5179d5c4fbcaa5fe547be587e9cece8a61372
Amirin <amirin@Amirins-MacBook-Pro.local>
2022-10-29 12:29:36 +0800

    added art and recommendation page

---
+++ pages/art-page.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>This is my art</h1>
+</template>
+
+<script>
+export default {
+    name : 'ArtPage'
+}
+</script>
+
---
+++ pages/recommendation.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Recommend me</h1>
+</template>
+
+<script>
+export default {
+    name : 'Recommendation'
+}
+</script>
+
